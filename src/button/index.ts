f68a7ffccbbe223a1b642f958f4c4366a34feca9
tyh2001 <1469442737@qq.com>
2022-10-25 17:42:57 +0800

    chore: 更新样式实现 🎡🎡🎡

--- src/button/index.ts
+++ src/button/index.ts
@@ -1,113 +1,72 @@
-import { buttonType, buttonSize } from './style'
-import { ChangeColor, defineProps } from '../_utils'
-import { Props } from './props'
-import type { ButtonType, ButtonSizeType } from './interface'
+// import { buttonType, buttonSize } from './style'
+// import { ChangeColor, defineProps } from '../_utils'
+// import { Props } from './props'
+// import type { ButtonType, ButtonSizeType } from './interface'
+// import { css, CSSResultGroup, html, nothing, TemplateResult } from 'lit'
 
 export class FButton extends HTMLElement {
   constructor() {
     super()
     this.setupShadow()
-
-    // const prop = defineProps(this)
-
-    // // 创建影子 dom
-    // // https://developer.mozilla.org/zh-CN/docs/Web/API/Element/attachShadow
-    // const shadowRoot: ShadowRoot = this.attachShadow({ mode: 'open' })
-
-    // // 创建实际的 button 元素
-    // const button: HTMLButtonElement = document.createElement('button')
-    // button.className = 'f-button'
-    // // 给 button 组件添加插槽
-    // const slot: HTMLSlotElement = document.createElement('slot')
-    // button.appendChild(slot)
-
-    // // console.log(Object.keys(this.attributes))
-
-    // // console.log(prop)
-
-    // console.log(this.getAttribute('type'))
-
-    // // const changeColor: ChangeColor = new ChangeColor(buttonType[prop.type])
-
-    // // 创建样式
-    // const style: HTMLStyleElement = document.createElement('style')
-
-    // // 为 shadow Dom 添加样式
-    // style.textContent = `
-    //   .f-button {
-    //     position: relative;
-    //     justify-content: center;
-    //     align-items: center;
-    //     border: none;
-    //     outline: none;
-    //     user-select: none;
-    //     text-decoration: none;
-    //     transition: 0.3s;
-    //     line-height: 1;
-    //     cursor: pointer;
-    //     overflow: hidden;
-    //     white-space: nowrap;
-    //     vertical-align: middle;
-    //     background: ${buttonType[prop.type]};
-    //     height: ${buttonSize[prop.size || 'middle'].height};
-    //     padding: 0 ${buttonSize[prop.size || 'middle'].padding};
-    //     font-size: ${buttonSize[prop.size || 'middle'].fontSize};
-    //     font-weight: ${prop.bold ? 'bold' : 'normal'};
-    //     border-radius: ${prop.round ? '30px' : '2px'};
-    //     display: ${prop.block ? 'block' : 'inline-flex'};
-    //     color: ${prop.color || prop.type === 'default' ? '#333' : '#fff'};
-    //   }
-
-
-    // `
-
-    // // 把样式和组件添加到影子 dom 中
-    // shadowRoot.appendChild(style)
-    // shadowRoot.appendChild(button)
   }
   /**
    * 为组件定义shadowDOM
    */
-  private setupShadow = () => {
-    const shadowDom: ShadowRoot = this.attachShadow({ mode: 'open' })
-    shadowDom.innerHTML = `
+  private setupShadow() {
+    // https://developer.mozilla.org/zh-CN/docs/Web/API/Element/attachShadow
+    const shadowRoot = this.attachShadow({ mode: 'open' })
+
+    // https://github.com/mdn/web-components-examples/blob/main/host-selectors/main.js
+    shadowRoot.innerHTML = `
+    <style>
+      :host {  
+        justify-content: center;
+        align-items: center;
+        border: none;
+        outline: none;
+        user-select: none;
+        text-decoration: none;
+        transition: 0.3s;
+        line-height: 1;
+        cursor: pointer;
+        overflow: hidden;
+        white-space: nowrap;
+        vertical-align: middle;
+        background: blue;
+        height: 35px;
+        border-radius: 2px;
+        display: inline-flex;
+        color: #fff;
+       }
+       .f-button {
+        background: transparent;
+        height: inherit;
+        line-height: 1;
+        width: inherit;
+        border: inherit;
+        box-sizing: border-box;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        color: inherit;
+        border-radius: inherit;
+        cursor: inherit;
+        font-weight: inherit;
+        font-size: inherit;
+        outline: none;
+      }
+    </style>
+    <button class="f-button">
       <slot></slot>
+    </button>
     `
   }
+
+  connectedCallback() {
+    console.log('首次调用')
+  }
+
+  attributeChangedCallback() {
+    console.log('增加属性')
+  }
 }
-
-
-/**
- * 
- *    .f-button:hover {
-        background: ${changeColor.getLightColor(0.3)};
-      }
-
-      .f-button:active {
-        background: ${changeColor.getDarkColor(0.3)};
-      }
-
-       <style>
-    .f-button {
-      position: relative;
-      justify-content: center;
-      align-items: center;
-      border: none;
-      outline: none;
-      user-select: none;
-      text-decoration: none;
-      transition: 0.3s;
-      line-height: 1;
-      cursor: pointer;
-      overflow: hidden;
-      white-space: nowrap;
-      vertical-align: middle;
-      background: blue;
-      height: 35px;
-      border-radius: 2px';
-      display: inline-flex;
-      color: #fff;
-    </style>
-
-   
- */
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { FButton } from './button/index'
 
 // https://developer.mozilla.org/zh-CN/docs/Web/API/CustomElementRegistry/define
+// https://developer.mozilla.org/zh-CN/docs/Web/Web_Components/Using_custom_elements
 customElements.define('f-button', FButton)
