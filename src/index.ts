8eb68cd37905494434c623a7dac11a978721f5ca
tyh2001 <1469442737@qq.com>
2022-10-25 10:36:15 +0800

    chore: 更新示例引入 🙀🙀🙀

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { FButton } from './button/index'
 
-// https://developer.mozilla.org/zh-CN/docs/Web/API/CustomElementRegistry/define
-customElements.define('f-button', FButton)
+const install = () => {
+  customElements.define('f-button', FButton)
+}
+
+export default install
--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import fightingWeb from '@fighting-design/web-components'
+
+fightingWeb()
+
 createApp(App).mount('#app')
