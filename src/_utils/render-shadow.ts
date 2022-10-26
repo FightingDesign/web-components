bf36765df5b8e3aa56eb6781e9dd05f8835c6cd6
tyh2001 <1469442737@qq.com>
2022-10-26 21:31:34 +0800

    style: 更新注释 🙀🙀🙀

--- src/_utils/render-shadow.ts
+++ src/_utils/render-shadow.ts
@@ -6,14 +6,30 @@ export class RenderShadow extends HTMLElement {
     super()
     this.setupShadow()
   }
-
+  /**
+   * 初始化影子节点
+   */
   setupShadow (): void {
+    /**
+     * 创建影子节点
+     * https://developer.mozilla.org/zh-CN/docs/Web/API/Element/attachShadow
+     */
     const shadowRoot: ShadowRoot = this.attachShadow({ mode: 'open' })
-    const sheet: CSSStyleSheet = new CSSStyleSheet()
 
+    // https://developer.mozilla.org/zh-CN/docs/Web/API/CSSStyleSheet
+    const stylesheet: CSSStyleSheet = new CSSStyleSheet()
+
+    // 渲染组件内部元素节点
     render(this.html(), shadowRoot)
-    sheet.replaceSync(this.css())
-    shadowRoot.adoptedStyleSheets = [sheet]
+
+    /**
+     * https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleSheet/replaceSync
+     * 必须传入字符串样式，不能传入路径
+     */
+    stylesheet.replaceSync(this.css())
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/adoptedStyleSheets
+    shadowRoot.adoptedStyleSheets = [stylesheet]
   }
 
   css (): string {
--- src/button/index.ts
+++ src/button/index.ts
@@ -9,26 +9,26 @@ export class FButton extends RenderShadow {
 
   css (): string {
     return `
-    :host {  
-      justify-content: center;
-      align-items: center;
-      border: none;
-      outline: none;
-      user-select: none;
-      text-decoration: none;
-      transition: 0.3s;
-      line-height: 1;
-      cursor: pointer;
-      overflow: hidden;
-      white-space: nowrap;
-      vertical-align: middle;
-      background: #2d5af1;
-      width: 105px;
-      height: 35px;
-      border-radius: 2px;
-      display: inline-flex;
-      color: #fff;
-     }
+      :host {  
+        justify-content: center;
+        align-items: center;
+        border: none;
+        outline: none;
+        user-select: none;
+        text-decoration: none;
+        transition: 0.3s;
+        line-height: 1;
+        cursor: pointer;
+        overflow: hidden;
+        white-space: nowrap;
+        vertical-align: middle;
+        background: #2d5af1;
+        width: 105px;
+        height: 35px;
+        border-radius: 2px;
+        display: inline-flex;
+        color: #fff;
+      }
     `
   }
 
