51ffac4d75045346bb40a642a4ec72e58d3961c9
tyh2001 <1469442737@qq.com>
2022-10-28 17:49:17 +0800

    chore: 更新配置项 😛😛😛

--- vite.config.ts
+++ vite.config.ts
@@ -13,14 +13,40 @@ export default (): UserConfigExport => {
       outDir: resolve(__dirname, './dist'),
       lib: {
         entry: resolve(__dirname, 'src/index.ts'),
-        formats: ['es'],
-        fileName: () => 'index.js'
+        name: 'FightingWeb' // 包名
       },
       rollupOptions: {
-        output: {
-          preserveModules: true
-        }
+        output: [
+          // {
+          //   format: 'umd',
+          //   exports: 'named',
+          //   sourcemap: false,
+          //   dir: 'dist/dist',
+          //   entryFileNames: 'index.umd.js',
+          //   chunkFileNames: '[name].js',
+          //   assetFileNames: '[name].[ext]',
+          //   namespaceToStringTag: true,
+          //   inlineDynamicImports: false
+          // },
+          {
+            format: 'es', // 打包模式 https://rollupjs.org/guide/en/#outputformat
+            exports: 'named', // 导出模式 https://rollupjs.org/guide/en/#outputexports
+            dir: 'dist/', // 输出路径 https://rollupjs.org/guide/en/#outputdir
+            sourcemap: false, // https://rollupjs.org/guide/en/#outputsourcemap
+            entryFileNames: 'index.js', // 输出后的文件名 https://rollupjs.org/guide/en/#outputentryfilenames
+            chunkFileNames: '[name].js', // 输出的 chunk文件名 https://rollupjs.org/guide/en/#outputchunkfilenames
+            assetFileNames: '[name].[ext]', // 输出资产文件名 https://rollupjs.org/guide/en/#outputassetfilenames
+            namespaceToStringTag: true, // https://rollupjs.org/guide/en/#outputnamespacetostringtag
+            inlineDynamicImports: false, // https://rollupjs.org/guide/en/#outputinlinedynamicimports
+            preserveModules: true // https://rollupjs.org/guide/en/#outputpreservemodules
+          }
+        ]
       }
+      // rollupOptions: {
+      //   output: {
+      //     preserveModules: true
+      //   }
+      // }
     }
   } as UserConfigExport
 }
